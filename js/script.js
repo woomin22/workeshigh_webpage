59161b2de5dd009de80628558f4d5dc0c4597a85
D-HYE <34275823+D-HYE@users.noreply.github.com>
2023-03-22 04:26:15 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#header_area').load('../includes/header.html')
+    $('#footer_area').load('../includes/footer.html')
+
+    $('.selected').click(function(){
+        let sel_has = $(this).parents('.select_wrap').hasClass('active');
+        $('.select_wrap').removeClass('active');
+        if(sel_has !== false){
+            $(this).parents('.select_wrap').removeClass('active');
+        }else{
+            $(this).parents('.select_wrap').addClass('active');
+        }
+    });
+    $('.select_wrap .select_option').click(function(){
+        let option_val = $(this).text();
+        $(this).closest('.select_wrap').find('.selected_value').text(option_val);
+        $(this).closest('.select_wrap').toggleClass('active');
+    });
+});
